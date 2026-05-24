cdb7c5eb12ed42090f1e656b3a5f65540862eaac
Foxciro <cnava28@gmail.coom>
2026-05-24 18:37:13 -0400

    refactor BASE_DIR definition in settings.py for improved clarity and consistency

--- simulaciones_pequiven/settings.py
+++ simulaciones_pequiven/settings.py
@@ -49,7 +49,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -237,6 +237,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
