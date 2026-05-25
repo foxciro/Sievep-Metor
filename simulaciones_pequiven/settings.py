2eabbb8cdfd30359bac64bb98b3b26226a4f1f71
Foxciro <cnava28@gmail.coom>
2026-05-24 20:23:10 -0400

    update STATICFILES_STORAGE to use CompressedStaticFilesStorage for improved performance

--- simulaciones_pequiven/settings.py
+++ simulaciones_pequiven/settings.py
@@ -275,7 +275,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
